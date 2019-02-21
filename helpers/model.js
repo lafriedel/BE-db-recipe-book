844f106dfb56a36b97659ea4fd894ab7378b022c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-21 18:10:04 -0500

    Complete data access file with exported functions

--- helpers/model.js
+++ helpers/model.js
@@ -3,29 +3,57 @@ const knexConfig = require("../knexfile");
 const db = knex(knexConfig.development);
 
 module.exports = {
-    getDishes,
-    addDish,
-    getDish,
-    getRecipes,
-    addRecipe
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe
 };
 
 function getDishes() {
-    return db('dishes').orderBy('id', 'asc');
-};
 
-function addDish(dish) {
+  return db("dishes")
+    .orderBy("id", "asc")
+    .limit(10);
 
-};
+}
+
+async function addDish(dish) {
+
+  const [id] = await db("dishes").insert(dish);
+
+  return id;
+
+}
 
 function getDish(id) {
 
-};
+  const dish = db("dishes")
+    .where("id", id)
+    .first();
+
+  const recipes = db("recipes")
+    .where("dish_id", id)
+    .orderBy("id", "asc");
+
+  return { dish, recipes };
+
+}
 
 function getRecipes() {
 
-};
+  return db("recipes")
+    .join("dishes", "dishes.name as dish")
+    .where("dishes.id", "recipes.dish_id")
+    .orderBy("id", "asc")
+    .limit(24);
 
-function addRecipe(recipe) {
+}
 
-};
+async function addRecipe(recipe) {
+
+  const [id] = await db("recipes").insert(recipe);
+
+  return id;
+  
+}
